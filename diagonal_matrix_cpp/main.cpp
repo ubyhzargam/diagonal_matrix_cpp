8dceae33f7f7e6df4ce8ce9c6a12316ca8e12343
ubyhzargam <ubyhzargam@gmail.com>
2023-07-01 12:06:44 +0530

    Initial Commit

---
+++ diagonal_matrix_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  diagonal_matrix_cpp
+//
+//  Created by Uby H on 01/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
